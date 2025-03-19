f5f2ff923986131f82b9a7a8fcfab30ef792a799
hungdv36 <hungdvph48701@gmail.com>
2025-03-19 12:51:46 +0700

    Cắt giao diện

--- commons/env.php
+++ commons/env.php
@@ -3,12 +3,15 @@
 // Biến môi trường, dùng chung toàn hệ thống
 // Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
 
-define('BASE_URL'       , 'http://localhost/mvc-oop-basic/');
+define('BASE_URL'       , 'http://localhost/DuAn1/duan1/');
+
+// Đường dẫn vào phầm admin
+define('BASE_URL_ADMIN'       , 'http://localhost/DuAn1/duan1/admin/');
 
 define('DB_HOST'    , 'localhost');
 define('DB_PORT'    , 3306);
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME'    , 'db');  // Tên database
+define('DB_NAME'    , 'duan1');  // Tên database
 
 define('PATH_ROOT'    , __DIR__ . '/../');
