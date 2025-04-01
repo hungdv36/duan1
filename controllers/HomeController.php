1969cf2f1ff1082ee9a09352aa8f12ae1802a39e
bao-21 <trankhacbao2104@gmail.com>
2025-04-01 09:45:25 +0700

    thanh toán

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -3,9 +3,14 @@
 class HomeController
 {
     public $modelSanPham;
+    public $modelDonHang;
+
+
     public function __construct()
     {
         $this->modelSanPham = new SanPham();
+        $this->modelDonHang = new DonHang();
+
     }
 
     public function home(){
@@ -21,4 +26,60 @@ class HomeController
         // var_dump($listProduct);die();
         require_once './views/listProduct.php';
     }
+    public function thanhToan(){
+        if (isset($_SESSION['user_client'])) {
+            $user = $this->modelTaiKhoan->getTaiKhoanFormEmail($_SESSION['user_client']);
+            // lấy dữ liệu giỏ hàng của người dùng
+
+            $gioHang = $this->modelGioHang->getGioHangFormEmail($user['id']);
+            if (!$gioHang) {
+                $gioHangId = $this->modelGioHang->addGioHang($user['id']);
+                $gioHang = ['id'=>$gioHangId];
+                $chiTietGioHang = $this->modelGioHang->getDetailGioHang($gioHang['id']);
+            } else {
+                $chiTietGioHang = $this->modelGioHang->getDetailGioHang($gioHang['id']);
+            }
+
+            require_once '.views/thanhToan.php';
+        }else {
+            var_dump('Chưa đăng nhập');die;
+        }
+    }
+
+    public function postThanhToan(){
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $ten_nguoi_nhan = $_POST['ten_nguoi_nhan'];
+            $email_nguoi_nhan = $_POST['email_nguoi_nhan'];
+            $sdt_nguoi_nhan= $_POST['sdt_nguoi_nhan'];
+            $dia_chi_nguoi_nhan = $_POST['dia_chi_nguoi_nhan'];
+            $ghi_chu = $_POST['ghi_chu'];
+            $tong_tien = $_POST['tong_tien'];
+            $phuong_thuc_thanh_toan_id = $_POST['phuong_thuc_thanh_toan_id'];
+
+            $ngay_dat = date('Y-m-d');
+            $trang_thai_id = 1;
+
+            $user = $this->modelTaiKhoan->getTaiKhoanFormEmail($_SESSION['user_client']);
+            $tai_khoan_id = $user['id'];
+
+            $ma_don_hang = 'DH-'. rand(1000,9999)
+
+            // Thêm trông tin vào DB
+
+            $this->modelDonHang->addDonHang($tai_khoan_id,
+                                            $ten_nguoi_nhan,
+                                            $email_nguoi_nhan,
+                                            $sdt_nguoi_nhan,
+                                            $dia_chi_nguoi_nhan,
+                                            $ghi_chu,
+                                            $tong_tien,
+                                            $phuong_thuc_thanh_toan_id,
+                                            $ngay_dat,
+                                            $ma_don_hang,
+                                            $trang_thai_id,
+            );
+
+        }
+    }
 }
+// sửa giỏ hàng header("Location: ". BASE_URL . '?act=login');
--- index.php
+++ index.php
@@ -11,15 +11,21 @@ require_once './controllers/HomeController.php';
 require_once './models/Student.php';
 require_once './models/SanPham.php';
 
+require_once './models/DonHang.php';
 // Route
 $act = $_GET['act'] ?? '/';
 
 // Để bảo bảo tính chất chỉ gọi 1 hàm Controller để xử lý request thì mình sử dụng match
 
 match ($act) {
+
     '/' => (new HomeController())->home(),
 
-    'trangchu' => (new HomeController())->trangchu(),
+    'thanh-toan' => (new HomeController())->thanhToan(),
+    'xu-ly-thanh-toan' => (new HomeController())->postThanhToan(),
 
-    'danh-sach-san-pham' =>(new HomeController())->danhSachSanPham(),
+
+    // 'trangchu' => (new HomeController())->trangchu(),
+
+    // 'danh-sach-san-pham' =>(new HomeController())->danhSachSanPham(),
 };
