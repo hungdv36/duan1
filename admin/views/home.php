1ef7cc6d0d28475bdce97cac8d79fc45dad23418
hungdv36 <hungdvph48701@gmail.com>
2025-04-06 22:39:22 +0700

    home

--- admin/views/home.php
+++ admin/views/home.php
@@ -1,35 +1,90 @@
-
 <?php include './views/layout/header.php'; ?>
-  <!-- Navbar -->
-  <?php include './views/layout/navbar.php'; ?>
+<!-- Navbar -->
+<?php include './views/layout/navbar.php'; ?>
 
-  <!-- /.navbar -->
+<!-- /.navbar -->
 
-  <!-- Main Sidebar Container -->
-  <?php include './views/layout/sidebar.php'; ?>
+<!-- Main Sidebar Container -->
+<?php include './views/layout/sidebar.php'; ?>
 
 
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Báo cáo thống kê</h1>
-            <?php var_dump($_SESSION['user_admin']); ?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1>Báo cáo thống kê</h1>
+          <?php var_dump($_SESSION['user_admin']); ?>
+        </div>
+      </div>
+    </div><!-- /.container-fluid -->
+  </section>
+
+  <!-- Main content -->
+  <section class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-3 col-6">
+          <div class="small-box bg-info">
+            <div class="inner">
+              <h3>150</h3>
+
+              <p>New Orders</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-bag"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
           </div>
         </div>
-      </div><!-- /.container-fluid -->
-    </section>
+        <div class="col-lg-3 col-6">
+          <div class="small-box bg-success">
+            <div class="inner">
+              <h3>53<sup style="font-size: 20px">%</sup></h3>
 
-    <!-- Main content -->
-    
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-  
+              <p>Bounce Rate</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <div class="col-lg-3 col-6">
+          <div class="small-box bg-warning">
+            <div class="inner">
+              <h3>44</h3>
+
+              <p>User Registrations</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person-add"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <div class="col-lg-3 col-6">
+          <div class="small-box bg-danger">
+            <div class="inner">
+              <h3>65</h3>
+
+              <p>Unique Visitors</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-pie-graph"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+      </div>
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
 <?php include './views/layout/footer.php'; ?>
 
 <!-- Page specific script -->
@@ -37,4 +92,5 @@
 <!-- Code injected by live-server -->
 
 </body>
+
 </html>
